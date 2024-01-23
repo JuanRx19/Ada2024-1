801ff5df4dc3abcef783a9585095693c21f93f3d
JuanRx19 <juanrx1904@gmail.com>
2024-01-23 11:23:55 -0500

    Archivos

--- Tarea 1/temp.py
+++ Tarea 1/temp.py
@@ -13,3 +13,33 @@ print(heapq.heappop(h))
 print(heapq.heappop(h))
 print(heapq.heappop(h))
 print(heapq.heappop(h))
+
+"""
+def solve(n, k):
+    med = []
+    tobby = []
+    suma = []
+    for i in range(n):
+        m, t = stdin.readline().split()
+        suma.append(int(t))
+        med.append((int(t), i, m))
+
+    iterator = 0
+    for cases in range(k):
+        if(iterator < n):
+            heapq.heappush(tobby, (suma[iterator], med[iterator][1], med[iterator][2]))
+            suma[iterator]+=med[iterator][0]
+            iterator+=1
+        else:
+            iterator = 0
+        
+    print(tobby)
+
+def main():
+    T = int(stdin.readline())
+    while(T>=1):
+        n, k = map(int, stdin.readline().split())
+        solve(n, k)
+        T = T - 1
+
+main()"""
--- Tarea 1/tobby.py
+++ Tarea 1/tobby.py
@@ -3,19 +3,31 @@ import heapq
 
 def solve(n, k):
     med = []
+    tobby = []
+    suma = []
     for i in range(n):
         m, t = stdin.readline().split()
-        med.append((m, int(t)))
-    tobby = med
+        suma.append(int(t))
+        med.append((int(t), i, m))
 
     iterator = 0
     for cases in range(k):
-        pass
-    print(med)
+        if(iterator < n):
+            heapq.heappush(tobby, (suma[iterator], med[iterator][1], med[iterator][2]))
+            suma[iterator]+=med[iterator][0]
+            iterator+=1
+        else:
+            heapq.heappush(tobby, (suma[iterator-1], med[iterator-1][1], med[iterator-1][2]))
+            suma[iterator-1]+=med[iterator-1][0]
+            iterator = 0 #El problema sigue aquí
+    
+    for _ in range(k):
+        desapel = heapq.heappop(tobby)
+        print(str(desapel[0]) + " " + desapel[2])
 
 def main():
     T = int(stdin.readline())
-    while(T>=1):
+    while(T>=1):            
         n, k = map(int, stdin.readline().split())
         solve(n, k)
         T = T - 1
