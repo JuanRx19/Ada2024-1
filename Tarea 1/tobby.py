240bc22bd384eae428048a19d1ef7a53dd10c47a
JuanRx19 <juanrx1904@gmail.com>
2024-01-22 20:58:54 -0500

    tobby - incompleto

---
+++ Tarea 1/tobby.py
@@ -0,0 +1,29 @@
+from sys import stdin
+import heapq
+
+def solve(n, k):
+    med = []
+    for i in range(n):
+        m, t = stdin.readline().split()
+        med.append((m, int(t)))
+    tobby = med
+
+    iterator = 0
+    for cases in range(k):
+        if(iterator < len(med) - 1):
+            ant = heapq.heappop(tobby)
+            heapq.hea
+            iterator+=1
+        else:
+            iterator=0
+        print(iterator)
+    print(med)
+
+def main():
+    T = int(stdin.readline())
+    while(T>=1):
+        n, k = map(int, stdin.readline().split())
+        solve(n, k)
+        T = T - 1
+
+main()
