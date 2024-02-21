0beaadaaeeae46c33f50d1410d0197a738b273e3
JuanRx19 <juanrx1904@gmail.com>
2024-02-21 16:35:14 -0500

    Diet

--- Tarea 3/diet.py
+++ Tarea 3/diet.py
@@ -1,7 +1,15 @@
 from sys import stdin
 
-def solve(calories, mem):
-    ans = None
+def solve(A, n, x):
+    ans = 0
+
+    if(n == 0 or x == 0):
+        ans = 0
+    elif(n != 0 and x != 0 and A[n-1] > x):
+        ans = solve(A, n-1, x)
+    elif(n != 0 and x != 0 and A[n-1] <= x):
+        ans = max(solve(A, n-1, x - A[n-1]) + A[n-1], solve(A, n-1, x))
+
     return ans
 
 def main():
@@ -9,7 +17,9 @@ def main():
     C = int(stdin.readline())
     while(C != 0):
         calories = int(stdin.readline())
-        print(f"{solve(calories, mem)}")
+        tam = int(stdin.readline())
+        A = list(map(int, input().split()))
+        print(f"{solve(A, tam, calories)}")
         C-=1
 
 main()
