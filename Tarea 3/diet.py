fb380d4b23724acb3f250ef094b167e956cf560c
JuanRx19 <juanrx1904@gmail.com>
2024-03-01 11:08:07 -0500

    Diet terminado, falta optimizar

--- Tarea 3/diet.py
+++ Tarea 3/diet.py
@@ -1,23 +1,29 @@
 from sys import stdin
 
-def solve(A, n, x):
+def solve(A, n, x, mem):
     ans = 0
-    if(n == 0 or x <= 0):
-        ans = 0
-    elif(n != 0 and x > 0):
-        ans = min(solve(A, n-1, x - A[n-1]) + A[n-1], solve(A, n-1, x) + A[n-1])
-
+    if((n, x) in mem):
+        ans = mem[(n, x)]
+    else:   
+        if(n == 0 and x > 0):
+            ans = -float('inf')
+        elif(n == 0 or x <= 0):
+            ans = x
+        elif(n != 0 and x > 0):
+            ans= max(solve(A, n-1, x - A[n-1], mem), solve(A, n-1, x, mem))
+            mem[(n, x)] = ans
     return ans
 
 def main():
     C = int(stdin.readline())
     while(C != 0):
+        mem = {}
         calories = int(stdin.readline())
         tam = int(stdin.readline())
         A = list(map(int, input().split()))
-        ans = solve(A, tam, calories)
-        if(ans >= calories):
-            print(f"{solve(A, tam, calories)}")
+        ans = solve(A, tam, calories, mem)
+        if(abs(ans) != float('inf')):
+            print(f"{calories + abs(ans)}")
         else:
             print("NO SOLUTION")
         C-=1
