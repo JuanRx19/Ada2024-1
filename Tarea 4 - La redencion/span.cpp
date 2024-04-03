6e579e2f7a43dac72cc8e452fa230061ec254551
JuanRx19 <juanrx1904@gmail.com>
2024-04-02 22:43:50 -0500

    Slim span resuelto

--- Tarea 4 - La redencion/span.cpp
+++ Tarea 4 - La redencion/span.cpp
@@ -1,96 +1,126 @@
 #include <stdio.h>
 #include <algorithm>
 #include <iostream>
+#include <vector>
 #include <cstring>
 #define inf 0x3f3f3f3f;
 using namespace std;
 #define MAX 100
+vector<int> p(100), rango(100);
 
-int padre[ MAX ];
-
-void MakeSet( int n ){
-    for( int i = 1 ; i <= n ; ++i ) padre[ i ] = i;
-}
-
-int Find( int x ){
-    return ( x == padre[ x ] ) ? x : padre[ x ] = Find( padre[ x ] );
-}
-
-void Union( int x , int y ){
-    padre[ Find( x ) ] = Find( y );
-}
-
-bool sameComponent( int x , int y ){
-    if( Find( x ) == Find( y ) ) return true;
-    return false;
-}
-
-int V , E;
-
-struct Edge{
+struct Arista{
     int origen;
     int destino;
     int peso;
-    Edge(){}
-    bool operator<( const Edge &e ) const {
-        return peso < e.peso;
+};
+
+bool cmpPeso(const Arista& a, const Arista& b){
+    return a.peso < b.peso;
+}
+
+void makeSet(int v){
+    p[v] = v;
+    rango[v] = 0;
+}
+
+int findSet(int v){
+    int ans = 0;
+    if(v == p[v]){
+        ans = v;
+    }else{
+        p[v] = findSet(p[v]);
+        ans = p[v];
     }
-}arista[ MAX ];
-Edge MST[ MAX ];
+    return ans;
+}
 
-int Kruskal(){
-    int start = 0;
-    int ans = inf;
-    bool flag = true;
-    while (flag)
-    {
-        int origen , destino , peso;
-        int total = 0;
-        int numAristas = 0;
-
-        MakeSet( V );
-        sort( arista , arista + E );
-
-        for( int i = start ; i < E ; ++i ){
-            origen = arista[ i ].origen;
-            destino = arista[ i ].destino;
-            peso = arista[ i ].peso;
-
-            if( !sameComponent( origen , destino ) ){
-                total += peso;
-                MST[ numAristas++ ] = arista[ i ];
-                Union( origen , destino );
-            }
+void unionSet(int u, int v){
+    u = findSet(u);
+    v = findSet(v);
+    if(u != v){
+        if(rango[u] < rango[v]){
+            u, v = v, u;
         }
-        if(V - 1 != numAristas){
+        p[v] = u;
+        if(rango[u] == rango[v]){
+            rango[u]++;
+        }
+    }
+}
+
+pair <vector<Arista>, int> Kruskal(int n, vector<Arista>& aristas, int start){
+    for (int i = 1; i < n + 1; i++) makeSet(i);
+    int INF = inf;
+    int numAristas = 0;
+    vector<Arista> mst(n-1);
+    for (int i = 0; i < n-1; i++) mst[i].peso = 0;
+    bool flag = true;
+
+    while(start != aristas.size() and flag){
+        if(findSet(aristas[start].origen) != findSet(aristas[start].destino)){
+            unionSet(aristas[start].origen, aristas[start].destino);
+            //cout << aristas[start].origen << " " << aristas[start].destino << endl;
+            //cout << findSet(aristas[start].origen) << " " << findSet(aristas[start].destino) << endl;
+            mst[numAristas] = aristas[start];
+            numAristas++;
+        }
+
+        if(numAristas == n-1){
             flag = false;
-        }else{
-            ans = min(ans, MST[numAristas-1].peso - MST[0].peso);
         }
         start++;
     }
     
+    return make_pair(mst, numAristas);
+}
+
+int solve(int n, int k, vector<Arista>& aristas){
+    int INF = inf;
+    int ans = inf;
+    int numAristas;
+    pair <vector<Arista>, int> Krus;
+    vector<Arista> arbol;
+    sort(aristas.begin(), aristas.end(), cmpPeso);
+    int start = 0;
+    bool flag = true;
+    while (flag)
+    {
+        Krus = Kruskal(n, aristas, start);
+        arbol = Krus.first;
+        numAristas = Krus.second;
+        if(numAristas != n-1){
+            flag = false;
+        }else{
+            ans = min(ans, arbol[arbol.size()-1].peso - arbol[0].peso);
+            start++;
+        }
+    }
+    
+    if(ans == INF){
+        ans = -1;
+        
+    }
     return ans;
 }
 
 int main(){
-    freopen("spanin.txt", "r", stdin);
-    freopen("spanout.txt", "w", stdout);
+    //freopen("spanin.txt", "r", stdin);
+    //freopen("spanout.txt", "w", stdout);
     int infinito = inf;
-    int ans = -1;
-    int start = 0;
-    while (cin >> V >> E, V != 0)
+    int ans;
+    int n, k;
+    while (cin >> n >> k, n != 0)
     {
-        for( int i = 0 ; i < E ; i++){
-            cin >> arista[i].origen >> arista[i].destino >> arista[i].peso;
+        vector<Arista> aristas(k);
+        for( int i = 0; i < k; i++){
+            cin >> aristas[i].origen >> aristas[i].destino >> aristas[i].peso;
         }
-
-        ans = Kruskal();
-        if(ans == infinito){
+        if(k == 0){
             cout << -1 << endl;
         }else{
-            cout << ans << endl;
+            cout << solve(n, k, aristas) << endl;
         }
+        
     }
     return 0;
 }
--- Tarea 4 - La redencion/span.py
+++ Tarea 4 - La redencion/span.py
@@ -3,20 +3,22 @@
 
 from sys import stdin
 
-def makeSet(v, p, rango):
+p, rango = [0 for _ in range(100)], [0 for _ in range(100)]
+
+def makeSet(v):
     p[v], rango[v] = v, 0
 
-def findSet(v, p, rango):
+def findSet(v):
     ans = None
     if v == p[v]:
         ans = v
     else:
-        p[v] = findSet(p[v], p, rango)
+        p[v] = findSet(p[v])
         ans = p[v]
     return ans
 
-def unionSet(u, v, p,  rango):
-    u, v = findSet(u, p, rango), findSet(v, p, rango)
+def unionSet(u, v):
+    u, v = findSet(u), findSet(v)
     if u != v:
         if rango[u] < rango[v]:
             u, v = v, u
@@ -24,29 +26,39 @@ def unionSet(u, v, p,  rango):
         if rango[u] == rango[v]:
             rango[u] += 1
 
-def kruskal(n, aristas, start, p, rango):
-    for i in range(1, n + 1): makeSet(i, p, rango)
+def kruskal(n, aristas, start):
+    for i in range(1, n + 1): makeSet(i)
     mst = []
-    for it in range(start, len(aristas)-1):
+    flag = 0
+    while(start != len(aristas)-1 and flag == 0):
+        u, v, c = aristas[start]
+        if findSet(u) != findSet(v):
+            unionSet(u, v)
+            mst.append(aristas[start])
+        
+        if(len(mst) == n-1):
+            flag = 1
+        start+=1
+    """for it in range(start, len(aristas)-1):
         u, v, c = aristas[it]
-        if findSet(u, p, rango) != findSet(v, p, rango):
-            unionSet(u, v, p, rango)
-            mst.append(aristas[it])
+        if findSet(u) != findSet(v):
+            unionSet(u, v)
+            mst.append(aristas[it])"""
+    print(mst)
     return mst
 
-def solve(n, k, grafo, p, rango):
+def solve(n, k, grafo):
     ans = float('inf')
     grafo.sort(key = lambda x: x[2])
     start = 0
     flag = 0
     while(flag == 0):
-        mst = kruskal(n, grafo, start, p, rango)
+        mst = kruskal(n, grafo, start)
         if(len(mst) < n-1):
             flag = 1
         else:
             ans = min(ans, mst[len(mst)-1][2] - mst[0][2])
             start+=1
-        k-=1
     #print(mst)
     if(ans == float('inf')):
         ans = -1
@@ -54,7 +66,6 @@ def solve(n, k, grafo, p, rango):
 
 def main():
     n, k = map(int, stdin.readline().split())
-    p, rango = [0 for _ in range(100)], [0 for _ in range(100)]
     while(n != 0):
         grafo = []
         for _  in range(k):
@@ -63,7 +74,7 @@ def main():
         if(k == 0):
             print("-1")
         else:
-            print(solve(n, k, grafo, p, rango))
+            print(solve(n, k, grafo))
         n, k = map(int, stdin.readline().split())
 
 main()
