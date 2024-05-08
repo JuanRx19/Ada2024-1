bdcaf57cd040fff629122a3a9882c62cb7f72050
JuanRx19 <juanrx1904@gmail.com>
2024-05-07 19:01:39 -0500

    Ya casi se corona eden

--- Tarea 5/eden.py
+++ Tarea 5/eden.py
@@ -3,36 +3,73 @@
 
 from sys import stdin
 
-def generate_rules(automata):
-  pass
+def binarytovalue(binary):
+  count = 0
+  potencia = 2
+  for bit in binary:
+    if(bit == '1'):
+      count+= 1 << potencia
+    potencia-=1
+
+  return count
 
 def automata(value):
     potencia = 7
-    data = []
+    data = [0] * 8
+    #print(data)
     while(potencia != -1):
         op = 1 << potencia
         if(op <= value):
           value -= op
-          data.append("1")
+          data[potencia] = '1'
         else:
-          data.append("0")
+          data[potencia] = '0'
         potencia-=1
     return data
 
-def conflict():
-  pass
+def getbits(prev):
+  temp = []
+  temp.append(prev[len(prev) - 2])
+  temp.append(prev[len(prev) - 1])
+  temp.append(prev[0])
+
+  return "".join(temp)
+
+def conflict(rules, bits, value):
+  ans = None
+
+  if(rules[binarytovalue(bits)] == value):
+    ans = True
+  else:
+    ans = False
+  
+  return ans
 
 def solve(rules, tam, cadena, i, prev):
   ans = None
   if(i == tam):
-    ans = True
+    ver1 = [prev[len(prev)-1], prev[0], prev[1]]
+    ver2 = [prev[0], prev[1], prev[2]]
+    #print("Entra")
+    if(conflict(rules, "".join(ver1), cadena[0]) and conflict(rules, "".join(ver2), cadena[1])):
+      #print(prev)
+      ans = True
+    else:
+      ans = False
   else:
     ans = False
-    for i in range(2):
-      prev.append(i)
-      if(i >= 2 and conflict()):
+    for bit in range(2):
+      if(bit == 0):
+        prev.append('0')
+      else:
+        prev.append('1')
+      if(i < 2):
+        ans = solve(rules, tam, cadena, i + 1, prev)
+      elif(i >= 2 and conflict(rules, getbits(prev), cadena[i])):
         ans = solve(rules, tam, cadena, i + 1, prev)
       prev.pop()
+  
+  return ans
 
 def main():
   line = stdin.readline().strip()
@@ -44,5 +81,7 @@ def main():
       print("GARDEN OF EDEN")
     line = stdin.readline().strip()
 
-print(automata(133))
-#main()
+#print(getbits('110'))
+#print(automata(121))
+#print(binarytovalue('101'))
+main()
